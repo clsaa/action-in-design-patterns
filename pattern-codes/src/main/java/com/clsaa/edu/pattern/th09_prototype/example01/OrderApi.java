9325e2eb6437bd9fb9a46a82e99ad36992e4cfe6
clsaa <812022339@qq.com>
2019-03-31 01:40:51 +0800

    原型模式

---
+++ pattern-codes/src/main/java/com/clsaa/edu/pattern/th09_prototype/example01/OrderApi.java
@@ -0,0 +1,17 @@
+package com.clsaa.edu.pattern.th09_prototype.example01;
+
+/**
+ * 订单的接口
+ */
+public interface OrderApi {
+	/**
+	 * 获取订单产品数量
+	 * @return 订单中产品数量
+	 */
+	public int getOrderProductNum();
+	/**
+	 * 设置订单产品数量
+	 * @param num 订单产品数量
+	 */
+	public void setOrderProductNum(int num);
+}
