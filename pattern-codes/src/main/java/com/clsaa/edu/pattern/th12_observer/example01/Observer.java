866677c06f233310c803b153da18431fe514fee2
clsaa <812022339@qq.com>
2019-04-01 13:20:11 +0800

    观察者模式

---
+++ pattern-codes/src/main/java/com/clsaa/edu/pattern/th12_observer/example01/Observer.java
@@ -0,0 +1,13 @@
+package com.clsaa.edu.pattern.th12_observer.example01;
+
+/**
+ * 观察者接口，定义一个更新的接口给那些在目标发生改变的时候被通知的对象
+ */
+public interface Observer {
+	/**
+	 * 更新的接口
+	 * @param subject 传入目标对象，好获取相应的目标对象的状态
+	 */
+	public void update(Subject subject);
+
+}
---
+++ pattern-codes/src/main/java/com/clsaa/edu/pattern/th12_observer/example02/Observer.java
@@ -0,0 +1,12 @@
+package com.clsaa.edu.pattern.th12_observer.example02;
+
+/**
+ * 观察者，比如报纸的读者
+ */
+public interface Observer {
+	/**
+	 * 被通知的方法
+	 * @param subject 具体的目标对象，可以获取报纸的内容
+	 */
+	public void update(Subject subject);
+}
